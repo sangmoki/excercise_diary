fceb2e2a4d6583c4dade6fc42caff34b9060e6ab
이상목 <yoochang4176@naver.com>
2024-06-01 13:32:28 +0900

    Feat: "firebase 익명 로그인 구성"

--- app/src/main/java/com/sangmoki/exercise_diary/SplashActivity.kt
+++ app/src/main/java/com/sangmoki/exercise_diary/SplashActivity.kt
@@ -3,12 +3,21 @@ package com.sangmoki.exercise_diary
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
+    // Firebase Auth 객체 생성
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,11 +28,35 @@ class SplashActivity : AppCompatActivity() {
             insets
         }
 
-        // 데이터 불러오는 동안 딜레이 시키기
-        Handler().postDelayed({
-            startActivity(
-                Intent(this, MainActivity::class.java))
-            finish()
-        }, 2000)
+        // Firebase Auth 객체 초기화
+        auth = Firebase.auth
+
+        // 사용자 현재 로그인 상태 확인
+        val currentUser = auth.currentUser
+
+        Log.d("LOGINTEST", currentUser!!.uid)
+
+//        Toast.makeText(this, "기존에 비회원 로그인이 되어있는 사람 !", Toast.LENGTH_SHORT,).show()
+
+        // 익명(비회원) 로그인 실행
+        auth.signInAnonymously()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(this, "비회원 로그인 성공 !", Toast.LENGTH_SHORT,).show()
+
+                    // 데이터 불러오는 동안 딜레이 시키기
+                    Handler().postDelayed({
+                        startActivity(
+                            Intent(this, MainActivity::class.java))
+                        finish()
+                    }, 2000)
+
+                } else {
+                    Toast.makeText(this,"비회원 로그인 실패 !", Toast.LENGTH_SHORT,).show()
+                }
+            }
+
+
+
     }
 }
