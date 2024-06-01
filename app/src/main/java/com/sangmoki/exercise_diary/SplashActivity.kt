a872735ef97384e6992042014cb1ab7c1d388307
이상목 <yoochang4176@naver.com>
2024-06-01 13:15:41 +0900

    Feat: "스플래시 화면 구현"

---
+++ app/src/main/java/com/sangmoki/exercise_diary/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.sangmoki.exercise_diary
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // 데이터 불러오는 동안 딜레이 시키기
+        Handler().postDelayed({
+            startActivity(
+                Intent(this, MainActivity::class.java))
+            finish()
+        }, 2000)
+    }
+}
