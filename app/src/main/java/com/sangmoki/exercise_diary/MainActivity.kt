d41c35b1e8b6eb745df39083f035f4639ba46e03
이상목 <yoochang4176@naver.com>
2024-06-02 18:38:52 +0900

    Feat: "dialog 내에서 버튼 이벤트 구현 및 다이얼로그 추가 띄우기 구현"

--- app/src/main/java/com/sangmoki/exercise_diary/MainActivity.kt
+++ app/src/main/java/com/sangmoki/exercise_diary/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.sangmoki.exercise_diary
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.DatePicker
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.GregorianCalendar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +27,41 @@ class MainActivity : AppCompatActivity() {
 
         val writeBtn = findViewById<ImageView>(R.id.writeBtn)
 
+        // 이미지 버튼 클릭 이벤트
         writeBtn.setOnClickListener {
 
+            // 커스텀 다이얼로그 객체 생성
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+                .setTitle("운동 기록 추가")
+
+            // 다이얼로그 띄움
+            val mAlertDialog = mBuilder.show()
+
+            // 띄운 다이얼로그에서 날짜 선택 버튼 클릭 이벤트
+            mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)?.setOnClickListener {
+
+                // 오늘 날짜의 년 월 일 구하기
+                val today = GregorianCalendar()
+                val year: Int = today.get(GregorianCalendar.YEAR)
+                val month: Int = today.get(GregorianCalendar.MONTH)
+                val day: Int = today.get(GregorianCalendar.DAY_OF_MONTH)
+
+                // 날짜 선택 다이얼로그 초기 데이터 설정
+                val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
+
+                    // 날짜 선택 시 이벤트
+                    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int
+                    ) {
+
+                    }
+
+                }, year, month, day)
+                
+                // 날짜 선택 다이얼로그 띄움
+                dlg.show()
+            }
         }
     }
 }
