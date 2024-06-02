e31f0e8f26564332ee553bf3200e754c9a7e6f6b
이상목 <yoochang4176@naver.com>
2024-06-02 18:42:47 +0900

    Refactor: "날짜 선택 Button Text 금일 날짜로 default 설정"

--- app/src/main/java/com/sangmoki/exercise_diary/MainActivity.kt
+++ app/src/main/java/com/sangmoki/exercise_diary/MainActivity.kt
@@ -30,6 +30,12 @@ class MainActivity : AppCompatActivity() {
         // 이미지 버튼 클릭 이벤트
         writeBtn.setOnClickListener {
 
+            // 오늘 날짜의 년 월 일 구하기
+            val today = GregorianCalendar()
+            val year: Int = today.get(GregorianCalendar.YEAR)
+            val month: Int = today.get(GregorianCalendar.MONTH)
+            val day: Int = today.get(GregorianCalendar.DAY_OF_MONTH)
+
             // 커스텀 다이얼로그 객체 생성
             val mDialogView = LayoutInflater.from(this).inflate(R.layout.custom_dialog, null)
             val mBuilder = AlertDialog.Builder(this)
@@ -39,14 +45,11 @@ class MainActivity : AppCompatActivity() {
             // 다이얼로그 띄움
             val mAlertDialog = mBuilder.show()
 
+            // 오늘 날짜로 날짜 선택 버튼 default 값 설정
+            mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)!!.setText("${year}년 ${month + 1}월 ${day}일")
+
             // 띄운 다이얼로그에서 날짜 선택 버튼 클릭 이벤트
             mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)?.setOnClickListener {
-
-                // 오늘 날짜의 년 월 일 구하기
-                val today = GregorianCalendar()
-                val year: Int = today.get(GregorianCalendar.YEAR)
-                val month: Int = today.get(GregorianCalendar.MONTH)
-                val day: Int = today.get(GregorianCalendar.DAY_OF_MONTH)
 
                 // 날짜 선택 다이얼로그 초기 데이터 설정
                 val dlg = DatePickerDialog(this, object : DatePickerDialog.OnDateSetListener {
@@ -54,7 +57,8 @@ class MainActivity : AppCompatActivity() {
                     // 날짜 선택 시 이벤트
                     override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int
                     ) {
-
+                        // 날짜 선택 시 버튼 텍스트 변경
+                        mAlertDialog.findViewById<Button>(R.id.dateSelectBtn)!!.setText("${year}년 ${month + 1}월 ${dayOfMonth}일")
                     }
 
                 }, year, month, day)
