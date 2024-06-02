67465972f10d5c4e317438c4541ed186c45f9413
이상목 <yoochang4176@naver.com>
2024-06-02 18:23:37 +0900

    Feat: "메모 버튼 생성 및 custom 해서 사용할 layout 생성"

--- app/src/main/java/com/sangmoki/exercise_diary/MainActivity.kt
+++ app/src/main/java/com/sangmoki/exercise_diary/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.sangmoki.exercise_diary
 
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +16,12 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val writeBtn = findViewById<ImageView>(R.id.writeBtn)
+
+        writeBtn.setOnClickListener {
+
         }
     }
 }
